cb595a40a792fdbae6cb493de36eb71eb5fdf12f
Tommy Cornilleau <tommy@grouek.com>
2015-03-31 16:46:31 +0200

    clean file + fix first light

--- main.js
+++ main.js
@@ -8,54 +8,23 @@ var spotLight;
 
 function init() {
 
-    // create a scene, that will hold all our elements such as objects, cameras and lights.
+    //// INIT
     scene = new THREE.Scene();
-
-    // create a camera, which defines where we're looking at.
     camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-    // create a render, sets the background color and the size
+    ////RENDERER
     renderer = new THREE.WebGLRenderer();
     renderer.setClearColor(0x000000, 1.0);
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.shadowMapEnabled = true;
 
-    // create the ground plane
-    var planeGeometry = new THREE.PlaneGeometry(80, 100, 15, 15);
-    var planeMaterial = new THREE.MeshLambertMaterial({color: 0xcccccc});
-    var plane = new THREE.Mesh(planeGeometry, planeMaterial);
-    plane.receiveShadow = true;
-
-    // rotate and position the plane
-    plane.rotation.x = -0.5 * Math.PI;
-    plane.position.x = 0;
-    plane.position.y = -50;
-    plane.position.z = 0;
-
-    // add the plane to the scene
-    scene.add(plane);
-
-    // create a cube
-    var cubeGeometry = new THREE.BoxGeometry(2, 2, 2);
-    var cubeMaterial = new THREE.MeshLambertMaterial({
-        wireframe: true,
-        color: 'white'
-    });
-    var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-    cube.position.z = 6;
-
-    cube.castShadow = true;
-
-    // add the cube to the scene
-    scene.add(cube);
-
-    // position and point the camera to the center of the scene
+    ////CAMERA
     camera.position.x = 0;
     camera.position.y = 30;
     camera.position.z = 0;
     camera.lookAt(scene.position);
 
-    
+    ////LIGHTS
     spotLight = new THREE.SpotLight(0xffffff);
     spotLight.intensity = 1;
     spotLight.angle = Math.PI;
@@ -64,20 +33,46 @@ function init() {
     spotLight.shadowCameraFar = 150;
     spotLight.castShadow = true;
 
-    scene.add(spotLight);
+    ambientLight = new THREE.AmbientLight(0x999999);
+    ambientLight.name = "ambientLight";
+
+    ////PLANE
+    var planeGeometry = new THREE.PlaneGeometry(80, 100, 15, 15);
+    var planeMaterial = new THREE.MeshLambertMaterial({color: 0x4444444});
+    var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+    plane.receiveShadow = true;
+    plane.rotation.x = -0.5 * Math.PI;
+    plane.position.x = 0;
+    plane.position.y = -50;
+    plane.position.z = 0;
+
+    ////CUBES
+    var cubeGeometry = new THREE.BoxGeometry(2, 2, 2);
+    var cubeMaterial = new THREE.MeshLambertMaterial({
+        wireframe: true,
+        color: 'white'
+    });
+    var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+    cube.position.z = 6;
+    cube.castShadow = true;
+    cube.transparent = true;
 
 
-
-
+    ////EXTRA
     control = new function() {
         this.camX = 0;
         this.camY = 30;
         this.camZ = 0;
         this.spotY = 30;
     };
-
-
     addControlStat(control);
+
+
+    ////ADDING
+    scene.add(plane);
+    scene.add(spotLight);
+    scene.add(ambientLight);
+    scene.add(cube);
 
 
     document.body.appendChild(renderer.domElement);
