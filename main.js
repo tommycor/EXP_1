b1ef9fe39891e369b022d3bfec3981d6cd2ecc76
Tommy Cornilleau <tommy@grouek.com>
2015-04-10 17:54:46 +0200

    Permet de mouvoir tout les cube + commentaire

--- main.js
+++ main.js
@@ -10,19 +10,17 @@ var maxV = 1500;
 var deltaV = minV - maxV;
 // global Forms
 var sizePlane = 200;
-var cube;
 var cubeSize = 2;
 var locate;
 //interaction
 var mouseX;
 var mouseZ;
-var tween;
 var timer;
 var mouseState;
 var maxR = Math.PI/3;
 //display
-var nbr_lines = 20;
-var nbr_columns = 20;
+var nbr_lines = 15;
+var nbr_columns = 15;
 var nbr_cubes = nbr_lines*nbr_columns;
 var margin = 7;
 var planeY = -50;
@@ -60,8 +58,8 @@ function init() {
     spotLight.angle = Math.PI/2;
     spotLight.shadowCameraNear = 10;
     spotLight.shadowDarkness = 1;
-    spotLight.shadowMapWidth = 2048;
-    spotLight.shadowMapHeight = 2048;
+    spotLight.shadowMapWidth = 3000;
+    spotLight.shadowMapHeight = 3000;
     spotLight.castShadow = true;
 
     ambientLight = new THREE.AmbientLight(0x999999);
@@ -79,7 +77,7 @@ function init() {
 
     var locateGeometry = new THREE.PlaneBufferGeometry(sizePlane, sizePlane);
     var locateMaterial = new THREE.MeshLambertMaterial({
-        color: 0xFF0000,
+        color: 0x000000,
         transparent : true,
         opacity: 0
     });
@@ -107,9 +105,6 @@ function init() {
         cubes[i].position.x= ((i % nbr_columns) * margin) + startPosX;
         cubes[i].position.z= (Math.floor(i/nbr_columns) * margin) + startPosZ;
     }
-    cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-    cube.castShadow = true;
-    cube.transparent = true;
 
     ////EXTRA
     control = new function(){
@@ -126,8 +121,6 @@ function init() {
     scene.add(locate);
     scene.add(spotLight);
     scene.add(ambientLight);
-    scene.add(cube);
-
     for( i=0 ; i<nbr_cubes; i++)
     {
         scene.add(cubes[i]);
@@ -140,17 +133,23 @@ function init() {
 }
 
 window.addEventListener('mousemove', function(event){
+    // Test l'état de la souris
     if(mouseState)
     {
+        // fonction qui va balancer les tweens qui vont remettre les cubes à leur état de repos
         toNormal();
         mouseState = false;
     }
+
+    // Permet de détecter si la souric ne bouge pas pendant plus d'une seconde
+    // A chaque fois que la souris bouge, un timer est lancé, et l'ancien est nettoyé
+    // followMouse est la fonction permettant de balancé un tween en direction de la souris
     clearTimeout(timer);
     timer = setTimeout(followMouse, 1000, event);
 
 });
 
-
+//RENDERER de base
 function render() {
     camera.position.x = control.camX;
     camera.position.y = control.camY;
@@ -166,6 +165,111 @@ function render() {
 }
 
 
+// Balance le tween pour que chaque cube retourne à l'état de repos
+function toNormal(){
+    for( i=0 ; i<nbr_cubes; i++)
+    {
+        // Si le tween est déjà défini et tourne, on le stop
+        if(tweens[i]) tweens[i].stop();
+        // Permet de stocker les valeurs du tween
+        currentRotX = cubes[i].rotation.x;
+        currentRotZ = cubes[i].rotation.z;
+        // TWEEN ne gère ni les tableaux de tween, ni les tableaux d'objets cibles
+
+        //On délègue la création des tween à customTweenNormal, et l'affectation de la rotation à updateTween
+        // On récupère le tween de manière à pouvoir le stopper comme il len faut lorsque la souris se stope
+        tweens[i] = customTweenNormal(currentRotX, currentRotZ, i);
+    }
+}
+
+// Balance le tween pour que chaque cube se tourne vers la souris
+function followMouse(event){
+    mouseState = true;
+
+    // On détecte la position de la souris
+    var vector = new THREE.Vector3( ( event.clientX / window.innerWidth ) * 2 - 1, - ( event.clientY / window.innerHeight ) * 2 + 1, 0.5 );
+    vector.unproject(camera);
+    // On balance le raycaster en fonction de la souris
+    var raycaster = new THREE.Raycaster(camera.position,vector.sub(camera.position).normalize() );
+    // On regarde les intersections entre le plan locate (invisible et au niveau des cubes) et le raycaster
+    var intersect = raycaster.intersectObject( locate );
+
+    mouseX = intersect[0].point.x;
+    mouseZ = intersect[0].point.z;
+
+    //  on boucle pour balancer le tween à chaque cube
+    for( i=0 ; i<nbr_cubes; i++)
+    {
+        // Si le tween est déjà défini et tourne, on le stop
+        if(tweens[i]) tweens[i].stop();
+
+
+        //DistanceAxeX = (Xa - Xb)
+        var distX = cubes[i].position.x - mouseX;
+        var distZ = cubes[i].position.z - mouseZ;
+
+        //Distance = racine((Xa - Xb)² + (Ya - Yb)²)
+        var dist = Math.sqrt(distX*distX + distZ*distZ);
+        // Paye ta formule relou mais qui marche
+        var timeRotate = deltaV * (4/sizePlane) * dist + maxV;
+        // Sinus
+        var rotZ = maxR * (distX/dist);
+        // Cosinus
+        var rotX = -maxR * (distZ/dist);
+        // On factorise ça de manière à ce que les plus proche tournent plus que les lointains
+        rotZ = dist*((rotZ/4-rotZ)/(sizePlane/4)) + rotZ;
+        rotX = dist*((rotX/4-rotX)/(sizePlane/4)) + rotX;
+
+        // customTweenFollow s'occupe de créer un tween à chaque fois qu'elle est appelé, avec en argument ce qu'il faut
+        // On récupère le tween de manière à pouvoir le stopper comme il len faut lorsque la souris rebouge
+        tweens[i] = customTweenFollow(rotZ, rotX, cubes[i].rotation.x, cubes[i].rotation.z, timeRotate, i);
+    }
+}
+
+// customTweenFollow s'occupe de créer un tween à chaque fois qu'elle est appelé
+// Ses arguments permettent de prendre en compte les variation de temps et tutti cuanti
+function customTweenFollow(rotZ, rotX, currentRotX, currentRotZ, duration, index){
+    // On va utiliser current values pour envoyer les variation de valeurs à updateTween
+    var currentValues = {x: null, z: null};
+
+    var tween = new TWEEN.Tween({x : currentRotX, z: currentRotZ})
+            .to({x : rotX, z: rotZ}, duration)
+            .onUpdate(function(){
+                currentValues.x = this.x;
+                currentValues.z = this.z;
+                // Puisque tween ne gère pas les tableaux, on va déléguer l'update à updateTween
+                // Les arguments sont les valeurs à affecter, et l'index du cube à modifier
+                updateTween(currentValues, index)
+            })
+            .easing(TWEEN.Easing.Circular.In)
+            .start();
+    return tween;
+}
+// customTweenFollow s'occupe de créer un tween à chaque fois qu'elle est appelé
+// Il possède des paramètres custom pour le retours à la normal du tween
+function customTweenNormal(currentRotX, currentRotZ, index){
+    // On va utiliser current values pour envoyer les variation de valeurs à updateTween
+    var currentValues = {x: null, z: null};
+
+    var tween = new TWEEN.Tween({x : currentRotX, z: currentRotZ})
+            .to({x : 0, z: 0}, 500)
+            .onUpdate(function(){
+                currentValues.x = this.x;
+                currentValues.z = this.z;
+                // Puisque tween ne gère pas les tableaux, on va déléguer l'update à updateTween
+                // Les arguments sont les valeurs à affecter, et l'index du cube à modifier
+                updateTween(currentValues, index)
+            })
+            .easing(TWEEN.Easing.Elastic.Out)
+            .start();
+    return tween;
+
+}
+// Update la rotation vien le onUpdate de Tween
+function updateTween(values, index){
+    cubes[index].rotation.x = values.x;
+    cubes[index].rotation.z = values.z;
+}
 
 function addControlStat(controlObject) {
     var gui = new dat.GUI();
@@ -185,81 +289,12 @@ function addControlStat(controlObject) {
 }
 
 
-
 function handleResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-
-function toNormal(){
-    for( i=0 ; i<nbr_cubes; i++)
-    {
-        if(tweens[i]) tweens[i].stop();
-
-        tweens[i] = new TWEEN.Tween({x : cubes[i].rotation.x, z: cubes[i].rotation.z})
-            .to({x : 0, z: 0}, 300)
-            .onUpdate(function(){
-                cubes[i].rotation.x = this.x;
-                cubes[i].rotation.z = this.z;
-            })
-            .easing(TWEEN.Easing.Elastic.Out)
-            .start();
-    }
-
-}
-
-function followMouse(event){
-    mouseState = true;
-
-    var vector = new THREE.Vector3( ( event.clientX / window.innerWidth ) * 2 - 1, - ( event.clientY / window.innerHeight ) * 2 + 1, 0.5 );
-    vector.unproject(camera);
-
-    var raycaster = new THREE.Raycaster(camera.position,vector.sub(camera.position).normalize() );
-    var intersect = raycaster.intersectObject( locate );
-
-    mouseX = intersect[0].point.x;
-    mouseZ = intersect[0].point.z;
-
-    for( i=0 ; i<nbr_cubes; i++)
-    {
-        if(tweens[i]) tweens[i].stop();
-
-        //DistanceAxeX = (Xa - Xb)
-        var distX = cubes[i].position.x - mouseX;
-        var distZ = cubes[i].position.z - mouseZ;
-
-        //Distance = racine((Xa - Xb)² + (Ya - Yb)²)
-        var dist = Math.sqrt(distX*distX + distZ*distZ);
-
-        var timeRotate = deltaV * (4/sizePlane) * dist + maxV;
-
-        var rotZ = maxR * (distX/dist);
-        var rotX = -maxR * (distZ/dist);
-
-                console.log(cubes[i])
-        rotZ = dist*((rotZ/4-rotZ)/(sizePlane/4)) + rotZ;
-        rotX = dist*((rotX/4-rotX)/(sizePlane/4)) + rotX;
-
-        tweens[i] = new TWEEN.Tween({x : cubes[i].rotation.x, z: cubes[i].rotation.z})
-            .to({x : rotX, z: rotZ}, timeRotate)
-            .onUpdate(function(){
-                cubes[i].rotation.x = this.x;
-                cubes[i].rotation.z = this.z;
-            })
-            .easing(TWEEN.Easing.Circular.In)
-            .start();
-    }
-
-    
-}
-
-function updateTween(tween, cube)
-{
-    cube.rotation.x = tween.x;
-    cube.rotation.z = tween.z;
-}
 
 
 
